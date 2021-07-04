27d5a4d52875927db86a053eb48ac17bbbd8c272
gettopwork <79124237+gettopwork@users.noreply.github.com>
2021-07-04 10:17:05 +0800

    Create gtmwebhook-sendData.js

---
+++ gtmwebhook-sendData.js
@@ -0,0 +1,14 @@
+function sendData(data, endpoint) {
+  var xhr = new XMLHttpRequest();
+  var stringifiedData = JSON.stringify(data);
+
+  xhr.open('POST', endpoint);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.send(stringifiedData);
+
+  xhr.onload = function () {
+    if (xhr.status.toString()[0] !== '2') {
+      console.error(xhr.status + '> ' + xhr.statusText);
+    }
+  };
+}
